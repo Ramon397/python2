18b03886b2c672d1a1bb71cedc1b4fea1d3bda3e
ramon <ramonpioparra@gmail.com>
2021-06-01 00:09:35 -0500

    "ac lista"

---
+++ helloworld.py
@@ -0,0 +1,28 @@
+print("Hello world")#Imprime mensaje por pantalla
+#strings
+print('''Hello world ''')
+print (type("Hello world"))
+print("Hola".upper()+"Mundo".upper())
+#numeros
+print (10)
+print (10.2)
+print (False)
+#list
+[10,20,30]
+print(["HOla","Agua",3,True])
+#tuplas
+(10,20,30)
+#diccionario
+print(type({"Nombre":"Ryan",
+"Apeido":"Rey",
+"SegundoApeido":"Fast"}))
+cadena="cadena so"
+print(cadena.replace('so' ,'hs'))#remplaza en palabra
+print (cadena.count('tamanño c'))#tamaño de variable
+print(cadena.startswith('hola'))#pregunatar sin empieeza con la palabra
+print(cadena.endswith('so'))#busca en la ultima palabra
+print(cadena.split())#separ por espacio
+print (cadena.find('c'))#posicion en cadena
+print(len(cadena))#tamaño de cadena
+print(cadena[3])# buscar al carater
+print(cadena[-1])# buscar al carater de atras para adelante
